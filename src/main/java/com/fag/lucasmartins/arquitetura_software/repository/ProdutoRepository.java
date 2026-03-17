dd347c1760e2ae438b6ce50b887ec1f39290c469
Dhstefanoski <dhstefanoski@minha.fag.edu.br>
2026-03-17 19:11:33 -0300

    Correções

---
+++ src/main/java/com/fag/lucasmartins/arquitetura_software/repository/ProdutoRepository.java
@@ -0,0 +1,6 @@
+package com.fag.lucasmartins.arquitetura_software.repository;
+import com.fag.lucasmartins.arquitetura_software.domain.Produto;
+
+public interface ProdutoRepository {
+    void salvar(Produto produto);
+}
